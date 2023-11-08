3d74d1fb2d33939c04eb31c54844c4abb7eb4022
AtroobaIQBAL <atroobaiqbal28@gmail.com>
2023-11-08 17:31:05 +0500

    first commit

---
+++ functions.js
@@ -0,0 +1,32 @@
+//#JS Fundamental Assignments
+
+//## Write a program that returns your age in days. (take age as an input)
+//function age(a) {
+//    return a;
+//}
+//console.log(age (21) * 365);
+
+//## Create a function that takes a number as an argument, increments the number 
+//by +1 and returns the result. (take number as an input)
+//function square(a) {
+//    return ++a;
+//}
+//console.log(square (5));
+
+//## Create a function that takes a number as an argument, 
+//decrements the number by -1 and returns the result.
+//function num (a) {
+//    return --a;
+//}
+//console.log(num (8));
+
+//## Create a function that takes a number and return square of a number
+//function squaree (a){
+//    return a * a;
+//}
+//console.log(squaree(7));
+
+//## Create a function that tells current time in Y-m-d format.
+
+
+//## Create a function  that tells current time from Date object in H:i:s format
